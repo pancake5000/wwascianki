a777460ec7d7fef471b95fc21cffd2ed304bd39c
pancake5000 <jkwiatkowski987@gmail.com>
2025-03-31 02:00:50 +0200

    added header

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          comic: ['"Comic Sans MS"', 'cursive'], // Add Comic Sans
+        },
+      },
+    },
+    plugins: [],
+  };
